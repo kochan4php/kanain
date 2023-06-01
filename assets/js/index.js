07bfbbf0ec4c2c41e3ebc6bef4392148b32cba41
kochan4php <aprodeosubarno@gmail.com>
2023-06-02 02:22:20 +0700

    membuat navbar responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
 const navbar = document.querySelector("header nav");
+const navBurger = document.querySelector(".nav-burger");
+const navList = document.querySelector(".nav-list");
 
 window.onscroll = function () {
     if (window.pageYOffset > 0) {
@@ -7,3 +9,7 @@ window.onscroll = function () {
         navbar.classList.remove("navbar-shadow");
     }
 };
+
+navBurger.addEventListener("click", function () {
+    navList.classList.toggle("slide");
+});
