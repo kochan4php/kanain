4c527e5e7eb6885e6b43bb1a0f0193fe19adf80c
kochan4php <aprodeosubarno@gmail.com>
2023-06-02 16:36:58 +0700

    membuat navbar ketika di scroll bawah menjadi hilang dan scroll atas menjadi muncul

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,21 @@ const navbar = document.querySelector("header nav");
 const navBurger = document.querySelector(".nav-burger i.fas.fa-bars");
 const navList = document.querySelector(".nav-list");
 
+let lastScrollTop;
+
 window.onscroll = function () {
     if (window.pageYOffset > 0) {
         navbar.classList.add("navbar-shadow");
     } else {
         navbar.classList.remove("navbar-shadow");
     }
+
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) navbar.style.top = "-100px";
+    else navbar.style.top = "0";
+
+    lastScrollTop = scrollTop;
 };
 
 navBurger.addEventListener("click", function () {
